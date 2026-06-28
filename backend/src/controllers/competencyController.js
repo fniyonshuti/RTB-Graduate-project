07415a2c85b6d490c286b35ff465ab675bf6114c
fniyonshuti <f.niyonshut1@alustudent.com>
2026-06-28 12:57:41 +0200

    refactor: simplify competency controller by delegating database operations to services

--- backend/src/controllers/competencyController.js
+++ backend/src/controllers/competencyController.js
@@ -1,65 +1,28 @@
-import Competency from '../models/Competency.js';
-import { AppError, asyncHandler } from '../utils/errors.js';
+import {
+  createCompetency as createCompetencyService,
+  getCompetencyForRole,
+  listCompetenciesForRole,
+  updateCompetencyById,
+} from '../services/competencyService.js';
+import { asyncHandler } from '../utils/errors.js';
 import { sendSuccess } from '../utils/response.js';
 
-function hideCorrectAnswers(competency) {
-  const data = competency.toObject ? competency.toObject() : competency;
-
-  return {
-    ...data,
-    theoryQuestions: (data.theoryQuestions || []).map((question) => {
-      const { correctAnswer, ...safeQuestion } = question;
-      return safeQuestion;
-    }),
-  };
-}
-
-function formatCompetencyForRole(competency, role) {
-  return role === 'graduate' ? hideCorrectAnswers(competency) : competency;
-}
-
 export const listCompetencies = asyncHandler(async (req, res) => {
-  const query = {};
-
-  if (req.query.activeOnly === 'true') query.isActive = true;
-  if (req.query.category) query.category = req.query.category;
-
-  const competencies = await Competency.find(query).sort({ category: 1, title: 1 });
-  sendSuccess(
-    res,
-    'Competencies loaded',
-    competencies.map((competency) => formatCompetencyForRole(competency, req.user.role))
-  );
+  const competencies = await listCompetenciesForRole(req.query, req.user.role);
+  sendSuccess(res, 'Competencies loaded', competencies);
 });
 
 export const getCompetency = asyncHandler(async (req, res) => {
-  const competency = await Competency.findById(req.params.id);
-
-  if (!competency) {
-    throw new AppError('Competency was not found', 404);
-  }
-
-  sendSuccess(res, 'Competency loaded', formatCompetencyForRole(competency, req.user.role));
+  const competency = await getCompetencyForRole(req.params.id, req.user.role);
+  sendSuccess(res, 'Competency loaded', competency);
 });
 
 export const createCompetency = asyncHandler(async (req, res) => {
-  const competency = await Competency.create({
-    ...req.body,
-    createdBy: req.user._id,
-  });
-
+  const competency = await createCompetencyService(req.body, req.user._id);
   sendSuccess(res, 'Competency created', competency, 201);
 });
 
 export const updateCompetency = asyncHandler(async (req, res) => {
-  const competency = await Competency.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-
-  if (!competency) {
-    throw new AppError('Competency was not found', 404);
-  }
-
+  const competency = await updateCompetencyById(req.params.id, req.body);
   sendSuccess(res, 'Competency updated', competency);
 });
